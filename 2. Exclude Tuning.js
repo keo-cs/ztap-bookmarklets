eb2b3bdfb040d83d7c91528e4c172ecb0585584c
keo-cs <110241893+keo-cs@users.noreply.github.com>
2022-11-01 21:08:02 -0500

    replace js files with formatted code

--- 2. Exclude Tuning.js
+++ 2. Exclude Tuning.js
@@ -1,1 +1,22 @@
-javascript:void((function() { function uEnc(str) { return encodeURIComponent(str).replace(/[!'()*]/g,(c) => `%${c.charCodeAt(0).toString(16).toUpperCase()}`) ;}; var uE = location.href; var uD = decodeURIComponent(location.href); var qStr = 'portal.threatanalytics.io/#/incidents?'; var param = ['product tuning %28criticalstart mdr%29', 'product tuning (criticalstart mdr)']; var paramN = param.length; if (uE.indexOf(qStr) == -1) { alert('This is not a ZTAP queue (URL: \''+uE+'\').'); return ;}; for (var i = 0; i < paramN; i++) { oc = uD.indexOf(param[i]); if (oc >= 0) { alert('Tuning events are already excluded (' + oc + ' : \'' + param[i] + '\').'); return ;} ;}; nU = uE + '&' + uEnc('-~Assigned Escalation Path') + '=' + uEnc(param[0]);location.href = nU; })());
+javascript:void((function(){
+  function uEnc(str) {
+    return encodeURIComponent(str).replace(/[!'()*]/g,(c) => `%${c.charCodeAt(0).toString(16).toUpperCase()}`)
+  }
+  var uE = location.href
+  var uD = decodeURIComponent(location.href)
+  var qStr = 'portal.threatanalytics.io/#/incidents?'
+  var param = ['product tuning %28criticalstart mdr%29', 'product tuning (criticalstart mdr)']
+  var paramN = param.length
+  if (uE.indexOf(qStr) == -1) {
+    alert('This is not a ZTAP queue (URL: \''+uE+'\').')
+    return
+  }
+  for (var i = 0; i < paramN; i++) {
+    oc = uD.indexOf(param[i])
+    if (oc >= 0) {
+      alert('Tuning events are already excluded (' + oc + ' : \'' + param[i] + '\').')
+      return 
+    }
+  }
+  nU = uE + '&' + uEnc('-~Assigned Escalation Path') + '=' + uEnc(param[0]);location.href = nU
+})());
